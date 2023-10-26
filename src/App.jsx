292e09edd6fbd5ea1f03e9b0bedb8c61d62337dd
Rishikesh <krishikesh369@gmail.com>
2023-10-26 23:04:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { MultiStep } from './MultiStep';
+
+function App() {
+  
+
+  return (
+    <MultiStep />
+  );
+}
+
+export default App
